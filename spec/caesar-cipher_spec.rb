7d0cf1c286dd9110e87eca943ca411d6a6399872
Dane Dalton <danezdalton1@gmail.com>
2022-08-31 11:49:56 -0500

    four unit tests for #shift_char

--- spec/caesar-cipher_spec.rb
+++ spec/caesar-cipher_spec.rb
@@ -9,3 +9,21 @@ describe '#caesar_cipher' do
     expect(caesar_cipher("Dane", -1)).to eql("Czmd")
   end
 end
+
+describe '#shift_char' do
+  it "shifts a character" do
+    expect(shift_char("a", 2)).to eql("c")
+  end
+
+  it "shifts capitalized characters" do
+    expect(shift_char("A", 2)).to eql("C")
+  end
+
+  it "shifts backwards" do
+    expect(shift_char("a", -2)).to eql("y")
+  end
+
+  it "shifts capitalized characters backwards" do
+    expect(shift_char("A", -2)).to eql("Y")
+  end
+end
